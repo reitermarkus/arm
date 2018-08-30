4033f44c9b799bcff99f1f85a71d12cdd2cfe2fe
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2018-08-30 23:27:04 +0200

    Add comments.

--- ibk.asm
+++ ibk.asm
@@ -4,18 +4,18 @@ msg:
   len = . - msg
 
 .align
-.global _start
+.global _start  ; make `_start` available.
 _start:
 /* write syscall */
-  MOV r0, #1
+  MOV r0, #1    ;output stream is monitor.
   LDR r1, =msg
-  LDR r2, =len
-  MOV r7, #4
+  LDR r2, =len  ;length of output string.
+  MOV r7, #4    ;output to screen.
   SWI #0
   B end
 
 /* exit syscall */
 end:
-  MOV r0, #0
-  MOV r7, #1
+  MOV r0, #0    ;input stream is keyboard.
+  MOV r7, #1    ;output to terminal
   SWI #0
